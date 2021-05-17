127091dc1a5589f4c22cca6f02dc4064c8bb04ed
davisvaceris <davis1234234@gmail.com>
2021-05-17 14:47:10 +0300

    div_chess

---
+++ static/styles/funk.js
@@ -0,0 +1,9 @@
+function move (laukums){
+    if(laukums.innerHTML==""){
+        laukums.innerHTML='<img src="images/rooda.svg" alt="rook dark" class="figura">'
+}
+else{
+        laukums.innerHTML="";
+    }
+}
+
