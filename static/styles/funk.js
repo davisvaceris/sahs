21fe2195bc3d877b62171871954550964bd2a666
davisvaceris <davis1234234@gmail.com>
2021-05-27 10:48:05 +0300

    saha_lauk

--- static/styles/funk.js
+++ static/styles/funk.js
@@ -1,6 +1,6 @@
 function move (laukums){
     if(laukums.innerHTML==""){
-        laukums.innerHTML='<img src="images/rooda.svg" alt="rook dark" class="figura">'
+        laukums.innerHTML='<img src="static/images/rooda.svg" alt="rook dark" class="figura">'
 }
 else{
         laukums.innerHTML="";
@@ -13,9 +13,11 @@ function parad() {
     var imag = document.getElementById("atttab");
 
         imag.style.cursor ="default";
-        x.style.display ="block";
+        x.style.display ="";
         y.style.display ="block";
         y.style.cursor ="pointer";
+        y.style.position ="fixed";
+
 }
 
 function slept(){
@@ -24,7 +26,27 @@ function slept(){
     var imag = document.getElementById("atttab");
     
         imag.style.cursor ="pointer";
-        x.style.display="none";
+        // x.style.display="none";
         y.style.display="none";
+        x.style.display="none";
 
+}
+
+function sakumla(){
+    var sak = document.getElementById("sakumlapa1");
+    var tab = document.getElementById("tabula1");
+
+    tab.style.cursor ="pointer"
+
+}
+
+function tabul(){
+    var sak = document.getElementById("sakumlapa1");
+    var tab = document.getElementById("tabula1");
+    var x = document.getElementById("sahalau");
+    var y = document.getElementById("krusts");
+
+    sak.style.cursor ="pointer"
+    y.style.display="none";
+    x.style.display="none";
 }
